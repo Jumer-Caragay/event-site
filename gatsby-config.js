21b495ed0de3c94c95b4ded2f031a93b7f74325d
Jumer Ryan Caragay <31534181+Jumer-Caragay@users.noreply.github.com>
2019-02-20 12:34:08 -0800

    fix(gatsby-config.js): remove gatsby-filesystem module (#1)

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,5 @@ module.exports = {
   pathPrefix: '/event-site',
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
   ],
 };
