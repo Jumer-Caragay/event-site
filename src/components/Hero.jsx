ff9d3661c39dc00e343519715ae1e4f0b5da7b6b
Jumer Ryan Caragay <31534181+Jumer-Caragay@users.noreply.github.com>
2019-02-20 17:09:17 -0800

    feat(src): setup layout (#3)
    * feat(src): setup layout
    
    * fix(src/pages): fix lint errors

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './hero.module.css';
+
+const Hero = () => (
+  <div className={styles.hero}>
+    <h1>This is the hero section</h1>
+  </div>
+);
+
+export default Hero;
