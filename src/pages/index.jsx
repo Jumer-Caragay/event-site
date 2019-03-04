47c888609aa05ed89f65c93355abf57d2181c3f2
Jumer Ryan Caragay <31534181+Jumer-Caragay@users.noreply.github.com>
2019-03-04 02:46:30 -0800

    feat(src): add home page (#4)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
+// import Navigation from '../components/Navigation';
 import './index.module.css';
+import './fonts.module.css';
 
 const IndexPage = () => (
   <div className="layout">
+    {/* <Navigation /> */}
     <Hero />
   </div>
 );
