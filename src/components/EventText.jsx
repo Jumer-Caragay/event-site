e452549e8962efa47c00a0d4fad3355732639bab
Jumer Ryan Caragay <31534181+Jumer-Caragay@users.noreply.github.com>
2019-03-11 03:48:29 -0700

    feat(src/components): add event description (#7)
    * feat(src/components): add event description
    
    * feat(EventText.jsx): fix lint errors

---
+++ src/components/EventText.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './eventText.module.css';
+
+const EventText = () => (
+  <div className={styles.eventText}>
+    <h1 className={styles.eventHeader}>
+      Envision beyond the bounds and discover
+      a limitless spectrum of possibilities.
+    </h1>
+    <p className={styles.eventParagraph}>
+      Envision is a 12-hour high school hackathon located at
+      California Polytechnic State University (Cal Poly)
+      in San Luis Obispo. We are bringing together
+      150 students all across the central coast from schools that have
+      limited access to computer science resources.
+    </p>
+  </div>
+);
+
+export default EventText;
